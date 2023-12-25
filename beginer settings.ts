72db70e328f240c7ea5ea7b7a595923d546b3630
Sam Alex <154799455+Sam666Alex@users.noreply.github.com>
2023-12-25 11:27:31 +0200

    Create beginer settings.ts

---
+++ beginer settings.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
+
+const config: Config = {
+
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+
+  plugins: [nextui()],
+}}
